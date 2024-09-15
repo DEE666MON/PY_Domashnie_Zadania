8af3dd2a29dd3fe8ba02f0f5309a3b6cfac350a6
Dmitriu <139161363+DEE666MON@users.noreply.github.com>
2024-09-15 11:49:14 +0300

    Add files via upload

--- module_5_hard_Dopolnitelnoe_zadanie_po_moduluy.py
+++ module_5_hard_Dopolnitelnoe_zadanie_po_moduluy.py
@@ -2,43 +2,80 @@ import time
 
 
 class Video:
+    time_now = 0
 
-    def __init__(self, title, duration, adult_mode=False):
+    def __init__(self, title: str, duration: int, adult_mode: bool = False):
         self.title = title
         self.duration = duration
         self.adult_mode = adult_mode
 
+    def __str__(self):
+        return self.title
+
 
 class User:
 
-    def __init__(self, nickname, password, age):
+    def __init__(self, nickname: str, password: str, age: int):
         self.nickname = nickname
         self.password = hash(password)
         self.age = age
 
+    def __str__(self):
+        return self.nickname
+
 
 class UrTube:
-    videos = []
-    users = []
+    videos: list[Video] = []
+    users: list[User] = []
     current_user = None
 
-    def add(self, *args):
-        pass
+    def register(self, nickname: str, password: str, age: int):
+        newUser = User(nickname, password, age)
+        for i in self.users:
+            if nickname == i.nickname:
+                print(f"Пользователь {nickname} уже существует")
+                return None
+        self.users.append(newUser)
+        self.log_in(nickname, password)
 
-    def get_videos(self, search_word):
-        pass
-
-    def watch_video(self, name_video):
-        pass
-
-    def log_in(self, nickname, password):
-        pass
-
-    def register(self, nickname, password, age):
-        pass
+    def log_in(self, nickname: str, password: str):
+        for i in self.users:
+            if i.nickname == nickname and i.password == hash(password):
+                self.current_user = i
+                break
 
     def log_out(self):
-        pass
+        self.current_user = None
+
+    def add(self, *args):
+        for i in args:
+            if i not in self.videos:
+                self.videos.append(i)
+
+    def get_videos(self, search_word: str):
+        videoTrue = []
+        search_word = search_word.lower()
+        for i in self.videos:
+            if search_word in i.title.lower():
+                videoTrue.append(i.title)
+        return videoTrue
+
+    def watch_video(self, search_video: str):
+        if not self.current_user:
+            print("Войдите в аккаунт, чтобы смотреть видео")
+            return None
+        for i in self.videos:
+            if search_video == i.title:
+                if i.adult_mode and self.current_user.age < 18:
+                    print("Вам нет 18 лет, пожалуйста покиньте страницу")
+                    break
+                else:
+                    for j in range(i.duration):
+                        i.time_now += 1
+                        print(i.time_now, end=' ')
+                        time.sleep(0.01)
+                    i.time_now = 0
+                    print("Конец видео")
 
 
 ur = UrTube()
