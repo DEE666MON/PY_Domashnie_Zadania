8ae7eb81684e52d3bcf80d1116b040ebd3d512e8
Dmitriu <139161363+DEE666MON@users.noreply.github.com>
2024-09-20 14:35:42 +0300

    Add files via upload

--- module_6_hard_Dopolnitelnoe_zadanie_po_moduluy.py
+++ module_6_hard_Dopolnitelnoe_zadanie_po_moduluy.py
@@ -4,18 +4,25 @@ import math
 class Figure:
     sides_count = 0
 
-    def __init__(self, color, *sides, filled):
-        self.__color = list(color)
-        if len(sides) == self.sides_count:
-            self.__sides = sides
+    def __init__(self, color, *sides):
+        if self.__is_valid_color(color):
+            self.__color = list(color)
         else:
+            self.__color = [0, 0, 0]
+        if len(sides) == self.sides_count:
+            self.__sides = list(sides)
+        elif len(sides) > 1 and len(sides) < self.sides_count:
             self.__sides = [1] * self.sides_count
-        self.filled = filled
+        else:
+            self.__sides = list(sides) * self.sides_count
+        self.filled = False
 
     def __is_valid_color(self, args):
         flag_proverka = False
         for i in args:
             flag_proverka = False
+            if not isinstance(i, int):
+                return False
             if i >= 0 and i <= 255:
                 flag_proverka = True
             else:
@@ -25,14 +32,17 @@ class Figure:
     def get_color(self):
         return self.__color
 
-    def set_color(self, *color):
-        if self.__is_valid_color(color):
-            self.__color = list(color)
+    def set_color(self, *new_color):
+        if not self.__is_valid_color(new_color):
+            return None
+        self.__color = list(new_color)
 
     def __is_valid_sides(self, args):
         flag_proverka = False
         for i in args:
             flag_proverka = False
+            if not isinstance(i, int):
+                return False
             if (len(args) == 1 or len(args) == self.sides_count) and i > 0:
                 flag_proverka = True
             else:
@@ -45,7 +55,9 @@ class Figure:
     def set_sides(self, *new_sides):
         if not self.__is_valid_sides(new_sides):
             return None
-        for i in new_sides:
+        if len(new_sides) == self.sides_count:
+            self.__sides = list(new_sides)
+        elif self.sides_count == 12:
             self.__sides = list(new_sides) * self.sides_count
 
     def __len__(self):
@@ -55,11 +67,11 @@ class Figure:
 class Circle(Figure):
     sides_count = 1
 
-    def __init__(self, color, sides):
-        newCircle = Figure(color, sides, filled=False)
-        self.set_color(*color)
-        self.set_sides(sides)
-        self.__radius = sides / 3.14 / 2
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
+        if len(sides) > 1:
+            self.set_sides(1)
+        self.__radius = self.get_sides()[0] / (2 * math.pi)
 
     def get_square(self):
         return float(3.14 * self.__radius ** 2)
@@ -68,24 +80,19 @@ class Circle(Figure):
 class Triangle(Figure):
     sides_count = 3
 
-    def __init__(self, color, sides):
-        newTriangle = Figure(color, sides, filled=False)
-        self.set_color(*color)
-        self.set_sides(sides)
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
 
     def get_square(self):
-        list_sides = self.get_sides()
-        p = float((1 / 2) * sum(list_sides))
-        return float(math.sqrt(p * (p - list_sides[0]) * (p - list_sides[1]) * (p - list_sides[2])))
+        p = float((1 / 2) * sum(self.get_sides()))
+        return float(math.sqrt(p * (p - self.get_sides()[0]) * (p - self.get_sides()[1]) * (p - self.get_sides()[2])))
 
 
 class Cube(Figure):
     sides_count = 12
 
-    def __init__(self, color, sides):
-        newCube = Figure(color, sides, filled=False)
-        self.set_color(*color)
-        self.set_sides(sides)
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
 
     def get_volume(self):
         for i in self.get_sides():
@@ -93,8 +100,7 @@ class Cube(Figure):
 
 
 circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
-triangle1 = Triangle((1, 0, 254), 3)
-cube1 = Cube((222, 35, 130), 6)
+cube1 = Cube((222, 35, 130), 6, 9)
 
 # Проверка на изменение цветов:
 circle1.set_color(55, 66, 77)  # Изменится
