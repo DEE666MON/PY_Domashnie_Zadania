a9e8d6989e64517042edccf556cf44a78a3d07db
Dmitriu <139161363+DEE666MON@users.noreply.github.com>
2024-10-08 13:28:11 +0300

    Add files via upload

--- module_10_1_Sozdanie_potokov.py
+++ module_10_1_Sozdanie_potokov.py
@@ -2,7 +2,21 @@ from time import sleep
 from datetime import datetime
 from threading import Thread
 
-time_start = datetime.now()
+time_start = 0
+time_end = 0
+
+
+def vremua_izmeritel(flag=False):
+    global time_start, time_end
+    if not flag:
+        time_start = datetime.now()
+        return None
+    time_end = datetime.now()
+    time_res = time_end - time_start
+    print(time_res)
+
+
+vremua_izmeritel()
 
 
 def write_words(word_count, file_name):
@@ -17,11 +31,10 @@ write_words(10, "example1.txt")
 write_words(30, "example2.txt")
 write_words(200, "example3.txt")
 write_words(100, "example4.txt")
-time_end = datetime.now()
-time_res = time_end - time_start
-print(time_res)
 
-time_start = datetime.now()
+vremua_izmeritel(True)
+vremua_izmeritel()
+
 thr_first = Thread(target=write_words, args=(10, "example5.txt"))
 thr_secod = Thread(target=write_words, args=(30, "example6.txt"))
 thr_third = Thread(target=write_words, args=(200, "example7.txt"))
@@ -34,6 +47,4 @@ thr_first.join()
 thr_secod.join()
 thr_third.join()
 thr_fourth.join()
-time_end = datetime.now()
-time_res = time_end - time_start
-print(time_res)
+vremua_izmeritel(True)
