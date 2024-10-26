f8b39da59057fb79de60d751f4c479658ce1d6f3
Dmitriu <139161363+DEE666MON@users.noreply.github.com>
2024-10-26 11:48:31 +0300

    Add files via upload

---
+++ module_11_3_Introspekciua.py
@@ -0,0 +1,40 @@
+import inspect
+import sys
+
+
+class Proverka:
+    flagProv = False
+
+    def __init__(self):
+        self.prov = 0
+
+    def proverka():
+        pass
+
+
+def introspection_info(obj):
+    slovar = {}
+    slovar["type"] = type(obj)
+    slovar["attributes"] = dir(obj)
+    slovar["methods"] = [i for i in dir(
+        obj) if inspect.ismethod(i) or inspect.isfunction(i)]
+    if inspect.isfunction(obj) or inspect.isclass(obj) or inspect.ismodule(obj):
+        slovar["module"] = inspect.getmodule(obj)
+    else:
+        slovar["module"] = __name__
+    slovar["size"] = sys.getsizeof(obj)
+    return slovar
+
+
+int_info = introspection_info(42)
+print(int_info, "\n")
+float_info = introspection_info(42.5)
+print(float_info, "\n")
+str_info = introspection_info("str")
+print(str_info, "\n")
+class_info = introspection_info(Proverka)
+print(class_info, "\n")
+bool_info = introspection_info(Proverka.flagProv)
+print(bool_info, "\n")
+def_info = introspection_info(introspection_info)
+print(def_info, "\n")
