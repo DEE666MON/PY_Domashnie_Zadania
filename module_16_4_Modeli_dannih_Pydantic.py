a2f56a32e0a159e3e8e76a955cd4326af379a761
Dmitriu <139161363+DEE666MON@users.noreply.github.com>
2024-12-21 13:47:41 +0300

    Add files via upload

--- module_16_4_Modeli_dannih_Pydantic.py
+++ module_16_4_Modeli_dannih_Pydantic.py
@@ -26,7 +26,7 @@ def get_users() -> List[User]:
 def post_users(
         username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
         age: int = Path(ge=18, le=120, description="Enter age", example="24")) -> User:
-    current_user = User(id=len(users)+1, username=username, age=age)
+    current_user = User(id=len(users) + 1, username=username, age=age)
     users.append(current_user)
     return current_user
 
@@ -43,14 +43,22 @@ def put_users(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User
                 U.age = age
                 return U
     except IndexError:
-        raise HTTPException(status_code=404, detail="Message not found.")
+        raise HTTPException(status_code=404, detail="User not found.")
 
 
 @app.delete("/user/{user_id}")
 def delete_users(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="50")]):
     try:
+        none_count = 0
         for U in users:
+            if users[U.id - 1].id == 0:
+                none_count += 1
             if user_id == U.id:
-                return users.pop(U.id-1)
+                temp = users[U.id - 1]
+                users[U.id - 1] = User(id=0, username="None", age=0)
+                return temp
+        if none_count == len(users):
+            users.clear()
+        raise IndexError
     except IndexError:
-        raise HTTPException(status_code=404, detail="Message not found.")
+        raise HTTPException(status_code=404, detail="User was not found.")
