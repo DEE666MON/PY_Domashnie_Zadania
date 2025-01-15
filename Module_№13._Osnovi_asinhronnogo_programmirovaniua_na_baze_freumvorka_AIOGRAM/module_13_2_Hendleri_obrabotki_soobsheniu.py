80b6592ddf7be45a86e6bdc0deb8bbe1c778075d
DEE666MON <DImaKamenev2000@Yandex.ru>
2025-01-15 20:19:51 +0300

    Начало работы.

--- Module_№13._Osnovi_asinhronnogo_programmirovaniua_na_baze_freumvorka_AIOGRAM/module_13_2_Hendleri_obrabotki_soobsheniu.py
+++ Module_№13._Osnovi_asinhronnogo_programmirovaniua_na_baze_freumvorka_AIOGRAM/module_13_2_Hendleri_obrabotki_soobsheniu.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "7858452054:AAESL7ff6uuQL23IaRV16tx_gmUA_KKBJb8"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
